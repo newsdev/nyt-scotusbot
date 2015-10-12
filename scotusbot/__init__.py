285dcb5ea5eded07a2a9cd6c823e7e131f65237c
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-10-12 13:39:07 -0400

    Sigh.

--- scotusbot/__init__.py
+++ scotusbot/__init__.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 
 CHANNEL = os.environ.get("SCOTUSBOT_SLACK_CHANNEL", None)
 MONGODB_CLIENT = MongoClient(os.environ.get('SCOTUSBOT_MONGO_URL', 'mongodb://localhost:27017/'))
-MONGODB_DATABASE = MONGODB_CLIENT.campfinbot
+MONGODB_DATABASE = MONGODB_CLIENT.scotusbot
